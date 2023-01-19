fb7ce098808bbada913a282a86feddd279af5d24
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-19 15:22:34 -0700

    solution

--- index.js
+++ index.js
@@ -12,57 +12,53 @@ function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
 };
 
 function createEmployeeRecords(records) {
-    let arr = [];
-    records.forEach(record => {
-        arr.push(createEmployeeRecord(record));
-    })
-    return arr;
+    return records.map(record => createEmployeeRecord(record))
 };
 
 function createTimeInEvent(record, dateStamp) {
-    let time = dateStamp.split(" ");
-    let timeInEvents = {
+    let [date, hour] = dateStamp.split(" ");
+    record.timeInEvents.push({
         type: "TimeIn",
-        hour: parseInt(time[1]),
-        date: time[0] 
-    }
-    record.timeInEvents.push(timeInEvents);
+        hour: parseInt(hour, 10),
+        date,
+    });
+
     return record;
 };
 
 function createTimeOutEvent(record, dateStamp) {
-    let time = dateStamp.split(" ");
-    let timeOutEvents = {
+    let [date, hour] = dateStamp.split(" ");
+    record.timeOutEvents.push({
         type: "TimeOut",
-        hour: parseInt(time[1]),
-        date: time[0] 
-    }
-    record.timeOutEvents.push(timeOutEvents);
+        hour: parseInt(hour, 10),
+        date,
+    });
+    
     return record;
 };
 
 function hoursWorkedOnDate(record, dateStamp) {
-    let timeInHour = record.timeInEvents[0].hour;
-    let timeOutHour = record.timeOutEvents[0].hour;
-    let hoursWorked = timeOutHour - timeInHour;
-    let str = hoursWorked.toString();
-    str = str.slice(0, -2);
-    str = parseInt(str);
-    
-    return str;
+    let timeInHour = record.timeInEvents.find(e => e.date === dateStamp)
+    let timeOutHour = record.timeOutEvents.find(e => e.date === dateStamp)
+    return (timeOutHour.hour - timeInHour.hour) / 100;
 };
 
 function wagesEarnedOnDate(record, dateStamp) {
-    let hoursWorked = hoursWorkedOnDate(record);
-    let payOwed = hoursWorked * record.payPerHour;
-    return payOwed;
+    let rawWage = hoursWorkedOnDate(record, dateStamp)
+    * record.payPerHour
+    return parseFloat(rawWage.toString())
 };
 
-function allWagesFor(record) {
-    console.log(record);
-    return totalPayOwed;
+function allWagesFor(employee) {
+    let eligibleDates = employee.timeInEvents.map(e => e.date);
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+}, 0)
+    return payable;
 };
 
 function calculatePayroll(records) {
-    
+    return records.reduce(function(memo, rec) {
+        return memo + allWagesFor(rec)
+    }, 0);
 };
